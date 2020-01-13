f689860c9ab46f8a0ae1710dfcd54e6a97fed19d
Meet mehta <meetujugal@gmail.com>
2020-01-13 23:17:55 +0530

    simple create table query

---
+++ queries.sql
@@ -0,0 +1,5 @@
+""" Author @MEET MEHTA """
+#varchar includes character and numbers, char includes only numbers, int includes numbers only.
+# CREATE, ALTER,SELECT,DELETE,UPDATE are different attributes used in database management. 
+CREATE table student( FirstName varchar(255), LastName(255), Id int, City varchar(255)); # Creates table with name=student with different columns
+
